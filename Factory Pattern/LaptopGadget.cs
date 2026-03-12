510c073277446a22e916c89f5329d714ca4d60f0
bhanu03042005 <bvchary345@gmail.com>
2026-03-12 14:52:14 +0530

    Add LaptopGadget class to GadgetHub namespace

---
+++ Factory Pattern/LaptopGadget.cs
@@ -0,0 +1,10 @@
+namespace GadgetHub 
+{
+    public class LaptopGadget : Gadget 
+    {
+       public LaptopGadget(string name){
+           this.gadgetName=name;
+           Console.WriteLine($"You have chosen Laptop Gadget and its name is {this.gadgetName}");
+       }
+    }
+}
