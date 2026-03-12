990f2e972092be8f18f4bc60dfa26fb37b6b1c2b
bhanu03042005 <bvchary345@gmail.com>
2026-03-12 14:51:35 +0530

    Add MobileGadget class to GadgetHub namespace

---
+++ Factory Pattern/MobileGadget.cs
@@ -0,0 +1,10 @@
+namespace GadgetHub
+{
+    public class MobileGadget : Gadget 
+    {
+       public MobileGadget(string name){
+           this.gadgetName=name;
+           Console.WriteLine($"You have chosen Mobile Gadget and its name is {this.gadgetName}");
+       }
+    }
+}
